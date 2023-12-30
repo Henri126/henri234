c9dc0fa22815a346a8d62164f70a58b311cbe586
Henri126 <118396736+Henri126@users.noreply.github.com>
2023-12-30 17:50:04 +0200

    0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const footer = document.querySelector('.footer');
+
+    const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+            footer.style.display = 'block';
+        } else {
+            footer.style.display = 'none'
+        }
+    });
+
+    observer.observe(document.querySelector('.footer'));
+});
